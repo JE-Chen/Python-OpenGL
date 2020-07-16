d79512787a6ff9a7d59308da98794298514da6f5
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-07-16 23:57:43 +0800

    Add files via upload

--- GLFW_Window.py
+++ GLFW_Window.py
@@ -2,28 +2,27 @@ import glfw
 
 class GLFW_Window():
 
-    def __init__(self):
-        pass
-
-    def Create_Window(self,Width=500,Height=500,Window_Name="GLFW_Window",X_Pos=200,Y_Pos=200):
-
+    def __init__(self,Width=500,Height=500,Window_Name="GLFW_Window",X_Pos=200,Y_Pos=200):
         if not glfw.init():
             raise Exception("Check GLFW")
 
-        Window = glfw.create_window(Width,Height,Window_Name,None,None)
+        self.Window = glfw.create_window(Width, Height, Window_Name, None, None)
 
-        if not Window :
+        if not self.Window:
+            glfw.terminate()
             raise Exception("Window Create Failure")
 
-        glfw.set_window_pos(Window,X_Pos,Y_Pos)
+        glfw.set_window_pos(self.Window, X_Pos, Y_Pos)
 
-        glfw.make_context_current(Window)
+        glfw.make_context_current(self.Window)
 
-        while not glfw.window_should_close(Window):
+    def Show_Window(self):
+
+        while not glfw.window_should_close(self.Window):
 
             glfw.poll_events()
 
-            glfw.swap_buffers(Window)
+            glfw.swap_buffers(self.Window)
 
         glfw.terminate()
 
