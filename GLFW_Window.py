4717df745de000d30bec637b483850ccf5940a0d
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-07-17 01:41:08 +0800

    Add files via upload

--- GLFW_Window.py
+++ GLFW_Window.py
@@ -1,9 +1,31 @@
 import glfw
 from OpenGL.GL import *
 
+
+
 class GLFW_Window():
 
+    def Default_Size_Change(self, Window, Width, Height):
+        print(Window, 'Width: {} Height: {}'.format(Width, Height))
+        glViewport(0,0,Width,Height)
+
+    def Set_Size_Change_Function(self, Function):
+        self.Default_Size_Function = Function
+
+    def Default_Close(self, Window):
+        print(Window,' Closed')
+
+    def Set_Close_Function(self, Function):
+        self.Default_Close_Function=Function
+
+    def Clear_Color(self, R=0.24, G=0.22, B=0.22, A=1):
+        glClearColor(R, G, B, A)
+
     def __init__(self,Width=500,Height=500,Window_Name="GLFW_Window",X_Pos=200,Y_Pos=200):
+
+        self.Default_Size_Function=self.Default_Size_Change
+        self.Default_Close_Function=self.Default_Close
+
         if not glfw.init():
             raise Exception("Check GLFW")
 
@@ -22,11 +44,11 @@ class GLFW_Window():
 
         glViewport(X_Pos, Y_Pos, Width, Height)
 
+        glfw.set_window_close_callback(self.Window,self.Default_Close_Function)
+
+        glfw.set_framebuffer_size_callback(self.Window,self.Default_Size_Function)
+
         self.Clear_Color()
-
-
-    def Clear_Color(self,R=0.24,G=0.22,B=0.22,A=1):
-        glClearColor(R,G,B,A)
 
     def Show_Window(self):
 
